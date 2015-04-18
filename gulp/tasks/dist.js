efe1e3b3923d10d600d5c8d675c19bad44f64a5f
Remie Bolte <r.bolte@gmail.com>
2015-04-18 23:28:16 +0200

    Added a file copy task to make sure the non-minified non-rollup version is also available in the dist folder

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -4,11 +4,16 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var config = require('../config').uglify;
 
-gulp.task('dist', ['uglify', 'browserify']);
+gulp.task('dist', ['copy', 'uglify', 'browserify']);
+
+gulp.task('copy', function() {
+	return gulp.src(config.src)
+			   .pipe(gulp.dest(config.dest));
+})
 
 gulp.task('uglify', function() {
 	return gulp.src(config.src)
 			   .pipe(uglify())
 			   .pipe(concat('tokensec.min.js'))
-			   .pipe(gulp.dest(config.dest))
+			   .pipe(gulp.dest(config.dest));
 });
